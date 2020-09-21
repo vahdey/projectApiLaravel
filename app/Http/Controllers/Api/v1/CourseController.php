db8bb2cc24b095ef529a47e9a8fdffffd4cf7ec3
vahdey <rasekhi_vahid@yahoo.com>
2020-09-21 15:02:33 +0330

    third upload

--- app/Http/Controllers/Api/v1/CourseController.php
+++ app/Http/Controllers/Api/v1/CourseController.php
@@ -7,6 +7,9 @@ use App\Http\Resources\v1\Course as CourseResource;
 use App\Http\Resources\v1\CourseCollection;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CourseRequest;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class CourseController extends Controller
 {
@@ -18,7 +21,34 @@ class CourseController extends Controller
 
     public function single(Course $course)
     {
-        throw new CourseIsPrivateException();
+        // throw new CourseIsPrivateException();
         return new CourseResource($course);
     }
+
+    public function store(CourseRequest $request) 
+    {
+
+        //        $this->validate($request , [
+        //            'title' => 'required|unique:courses|max:255',
+        //            'body' => 'required'
+        //        ]);
+        
+        //        $validator = Validator::make($request->all() , [
+        //            'title' => 'required|unique:courses|max:255',
+        //            'body' => 'required'
+        //        ]);
+        //
+        //        if($validator->fails()) {
+        //            return response([
+        //                'data' => $validator->errors(),
+        //                'status' => 'error'
+        //            ], 422);
+        //        }
+        
+                return response([
+                    'data' => [],
+                    'status' => 'success'
+                ],200);
+        
+            }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ use Illuminate\Http\Request;
 Route::prefix('v1')->namespace('Api\v1')->group(function() {
     Route::get('/courses' , 'CourseController@index');
     Route::get('/courses/{course}' , 'CourseController@single');
+    Route::post('/courses' , 'CourseController@store');
 });
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+
